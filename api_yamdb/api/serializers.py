8c63fa99eb0508965a445e1ec1f736a8b78f099e
Игорь Бутаков <bikrtw@gmail.com>
2022-03-30 14:09:49 +0300

    fix Title serializer to match documentation

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from reviews import models
 
@@ -17,33 +18,32 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class TitleSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
-    category = CategorySerializer(read_only=True)
-    genre = GenreSerializer(read_only=True, many=True)
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=models.Category.objects.all())
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', queryset=models.Genre.objects.all(), many=True)
 
     class Meta:
         model = models.Title
         fields = ('id', 'name', 'year', 'rating', 'description', 'category',
                   'genre')
+        required_fields = ('name', 'year', 'category', 'genre')
 
     def get_rating(self, obj):
         return 0  # TODO implement
 
     def create(self, validated_data):
         category = validated_data.pop('category')
-        genre = validated_data.pop('genre')
+        genres = validated_data.pop('genre')
 
-        # creating title
+        # create title
         title = models.Title.objects.create(**validated_data)
 
-        # creating category
-        category_obj, created = models.Category.objects.get_or_create(
-            name=category['name'], slug=category['slug'])
-        title.category.add(category_obj)
+        # set category
+        title.category = category
 
-        # creating genre
-        for g in genre:
-            genre_obj, created = models.Genre.objects.get_or_create(
-                name=g['name'], slug=g['slug'])
-            title.genre.add(genre_obj)
+        # add genres
+        for genre in genres:
+            title.genre.add(genre)
 
         return title
