9210f55c83dfcbea5e38e28e3940320c39e86969
Игорь Бутаков <bikrtw@gmail.com>
2022-03-30 13:22:51 +0300

    add reviews viewsets

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
 
-# Create your views here.
+
+from . import serializers, models, permissions as review_permissions
+
+
+class CreateListDestroy(mixins.CreateModelMixin, mixins.ListModelMixin,
+                        mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+class GenreViewSet(CreateListDestroy):
+
+    class Meta:
+        model = models.Genre
+        serializer_class = serializers.GenreSerializer
+        permission_classes = [review_permissions.IsAdminOrReadOnly]
+
+
+class CategoryViewSet(CreateListDestroy):
+
+    class Meta:
+        model = models.Category
+        serializer_class = serializers.CategorySerializer
+        permission_classes = [review_permissions.IsAdminOrReadOnly]
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+
+    class Meta:
+        model = models.Title
+        serializer_class = serializers.TitleSerializer
+        permission_classes = [review_permissions.IsAdminOrReadOnly]
