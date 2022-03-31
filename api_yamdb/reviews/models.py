bc7d7878961ef823181a221954e0adb37cb504f1
Игорь Бутаков <bikrtw@gmail.com>
2022-03-31 15:40:53 +0300

    add unique constraint to slug field for Genre and Category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
 
 
 class Title(models.Model):
@@ -23,5 +23,5 @@ class Title(models.Model):
         Category,
         related_name='titles',
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
     )
