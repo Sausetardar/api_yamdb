12cc22c65d9605658e078d270cc4c703db13e10e
Максим Усенко <maksimusemko@inbox.ru>
2022-04-01 15:17:57 +0300

    model user

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    SADMIN = 'sadmin'
+    ROLES = (
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+        (SADMIN, 'sadmin'),
+    )
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    role = models.CharField(max_length=30, choices=ROLES, default='user')
+    confirmation_code = models.CharField(max_length=200, default='FOOBAR')
