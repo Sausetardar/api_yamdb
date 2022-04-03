f4152e471ab8035b97316d99e02bf4664eff9372
Игорь Бутаков <bikrtw@gmail.com>
2022-04-03 19:16:13 +0300

    fix permissions to work with new User models

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions
+from rest_framework import permissions, exceptions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -6,7 +6,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user and request.user.is_staff
+        return (request.user.is_authenticated
+                and request.user.role in ['admin', 'sadmin'])
 
 
 class ReviewCommentPermission(permissions.BasePermission):
