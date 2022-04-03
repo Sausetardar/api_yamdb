f01d270fbc4e9e1137f624a99af73525f7635118
Алексей Кузьмин <akredeemer@yandex.ru>
2022-04-03 15:08:21 +0300

    Add Review and Comment features

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,3 +12,20 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
+
+
+class ReviewCommentPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE' and request.user.role == 'moderator':
+            return True
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+        )
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,8 +7,17 @@ router = DefaultRouter()
 
 router.register('genres', views.GenreViewSet)
 router.register('categories', views.CategoryViewSet)
-router.register('titles', views.TitleViewSet)
-
+router.register('titles', views.TitleViewSet, basename='titles')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    views.ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    views.CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
