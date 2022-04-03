94c622381790de258b4e63037d6f7fa9456f73ed
Игорь Бутаков <bikrtw@gmail.com>
2022-04-03 15:52:34 +0300

    fix readme md + pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -106,6 +106,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 AUTH_USER_MODEL = 'reviews.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
