a1b7e16292e13c442c0804ab24611c152d5c9088
Максим Усенко <94329761+Sausetardar@users.noreply.github.com>
2022-04-06 15:43:54 +0300

    Update permission.py

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -14,15 +14,13 @@ class ReviewCommentPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-        )
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.method == 'DELETE' and request.user.is_moderator:
             return True
         return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-        )
+                or obj.author == request.user)
 
 
 class IsAdmin(permissions.BasePermission):
